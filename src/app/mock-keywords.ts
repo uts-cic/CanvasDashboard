96012af70b0761cd8f364c64a5cd5de2a0399738
jessicawiradinata <jessicawiradinata@gmail.com>
2017-11-14 12:01:02 +1100

    Fixed keywords list height to fixed and scrolled overflow

--- src/app/mock-keywords.ts
+++ src/app/mock-keywords.ts
@@ -3,8 +3,12 @@ export const KEYWORDS = [
   { word: 'DesignPrinciples', total: 34 },
   { word: 'MobileDevelopment', total: 25 },
   { word: 'SoftwareDevelopment', total: 16 },
+  { word: 'IXD', total: 15 },
+  { word: 'IXDEvent2017', total: 13 },
   { word: 'GamesDevelopment', total: 7 },
   { word: 'DesignLearning', total: 5 },
+  { word: 'UTS', total: 5 },
   { word: 'CreativeDesign', total: 4 },
+  { word: 'Sydney', total: 3 },
   { word: 'Music', total: 1 }
 ];
