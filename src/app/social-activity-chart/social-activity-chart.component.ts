f16886c935a04bdd5542c1356148828c58475cfb
jessicawiradinata <jessicawiradinata@gmail.com>
2017-11-17 10:26:33 +1100

    Social activity chart uses dates

--- src/app/social-activity-chart/social-activity-chart.component.ts
+++ src/app/social-activity-chart/social-activity-chart.component.ts
@@ -30,17 +30,19 @@ export class SocialActivityChartComponent implements OnInit {
         y: function(d){ return d.y; },
         useInteractiveGuideline: true,
         dispatch: {
-            stateChange: function(e){ console.log('stateChange'); },
-            changeState: function(e){ console.log('changeState'); },
-            tooltipShow: function(e){ console.log('tooltipShow'); },
-            tooltipHide: function(e){ console.log('tooltipHide'); }
+          stateChange: function(e){ console.log('stateChange'); },
+          changeState: function(e){ console.log('changeState'); },
+          tooltipShow: function(e){ console.log('tooltipShow'); },
+          tooltipHide: function(e){ console.log('tooltipHide'); }
         },
         xAxis: {
-            axisLabel: 'Timeline'
+          showMaxMin: false,
+          axisLabel: 'Timeline',
+          tickFormat: d => d3.time.format('%x')(new Date(d)),
         },
         yAxis: {
-            axisLabel: 'Activities',
-            axisLabelDistance: -10
+          axisLabel: 'Activities',
+          axisLabelDistance: -10
         },
       }
     };
@@ -49,11 +51,33 @@ export class SocialActivityChartComponent implements OnInit {
 
     function sinAndCos() {
       const sin = [], cos = [], cos2 = [];
+      const dates = [
+        1025409600000,
+        1028088000000,
+        1030766400000,
+        1033358400000,
+        1036040400000,
+        1038632400000,
+        1041310800000,
+        1043989200000,
+        1046408400000,
+        1049086800000,
+        1051675200000,
+        1054353600000,
+        1056945600000,
+        1059624000000,
+        1062302400000,
+        1064894400000,
+        1067576400000,
+        1070168400000,
+        1072846800000,
+        1075525200000
+      ];
 
-      for (let i = 0; i < 100; i++) {
-        sin.push({x: i, y: Math.round(Math.abs(Math.sin(i / 10) * 2) * 35)});
-        cos.push({x: i, y: Math.round(Math.abs(Math.cos(i / 10 + 2) + Math.random() / 10) * 50)});
-        cos2.push({x: i, y: Math.round(Math.abs(Math.cos(i / 10 + 2) + Math.random() / 10) * 30)});
+      for (let i = 0; i < dates.length; i++) {
+        sin.push({x: dates[i], y: Math.round(Math.abs(Math.sin(i / 10) * 2) * 35)});
+        cos.push({x: dates[i], y: Math.round(Math.abs(Math.cos(i / 10 + 2) + Math.random() / 10) * 50)});
+        cos2.push({x: dates[i], y: Math.round(Math.abs(Math.cos(i / 10 + 2) + Math.random() / 10) * 30)});
       }
 
       return [
