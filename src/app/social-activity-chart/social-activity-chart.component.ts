634882b42a45d7c1675342791419c964c4db1e40
jessicawiradinata <jessicawiradinata@gmail.com>
2017-11-17 11:42:29 +1100

    Add and remove social activity chart data using toggle button

--- src/app/social-activity-chart/social-activity-chart.component.ts
+++ src/app/social-activity-chart/social-activity-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, ViewChild } from '@angular/core';
 
 declare let d3: any;
 
@@ -11,6 +11,8 @@ declare let d3: any;
 export class SocialActivityChartComponent implements OnInit {
   private options: any;
   private data: any;
+  public isShow = false;
+  @ViewChild('nvd3') nvd3;
 
   constructor() {
   }
@@ -21,10 +23,10 @@ export class SocialActivityChartComponent implements OnInit {
         type: 'lineChart',
         height: 450,
         margin : {
-            top: 20,
-            right: 20,
-            bottom: 40,
-            left: 55
+          top: 20,
+          right: 20,
+          bottom: 40,
+          left: 55
         },
         x: function(d){ return d.x; },
         y: function(d){ return d.y; },
@@ -98,5 +100,48 @@ export class SocialActivityChartComponent implements OnInit {
         },
       ];
     }
+  }
+
+  toggleShow() {
+    this.isShow = !this.isShow;
+
+    if (this.isShow) {
+      const newData = [];
+      const dates = [
+        1025409600000,
+        1028088000000,
+        1030766400000,
+        1033358400000,
+        1036040400000,
+        1038632400000,
+        1041310800000,
+        1043989200000,
+        1046408400000,
+        1049086800000,
+        1051675200000,
+        1054353600000,
+        1056945600000,
+        1059624000000,
+        1062302400000,
+        1064894400000,
+        1067576400000,
+        1070168400000,
+        1072846800000,
+        1075525200000
+      ];
+      for (let i = 0; i < dates.length; i++) {
+        newData.push({x: dates[i], y: Math.round(Math.abs(Math.cos(i / 10 + 2) + Math.random() / 10) * 10)});
+      }
+      this.data.push({
+        values: newData,
+        key: 'InteractionDesign',
+        area: true
+      });
+    } else {
+      this.data.splice(3, 1);
+    }
+
+    this.nvd3.chart.update();
+
   }
 }
