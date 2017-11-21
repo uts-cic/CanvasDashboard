2bdf26a39ee6e1c61bf7ba4b3a51baeb7e678ccb
jessicawiradinata <jessicawiradinata@gmail.com>
2017-11-22 10:17:01 +1100

    Change navigation from tabs to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PersonalComponent } from './containers/personal/personal.component';
 import { SocialComponent } from './containers/social/social.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'social', component: SocialComponent },
+  { path: 'personal', component: PersonalComponent }
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
