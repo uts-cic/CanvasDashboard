83af9bcef434cdf2548e21ff0bb0cec210c4d16c
jessicawiradinata <jessicawiradinata@gmail.com>
2017-11-22 14:27:03 +1100

    Setup logic for first row content

--- src/app/containers/social/social.component.ts
+++ src/app/containers/social/social.component.ts
@@ -8,13 +8,20 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   encapsulation: ViewEncapsulation.None
 })
 export class SocialComponent implements OnInit {
-  public socialActivityGroup = false;
-  public networkAnalysisGroup = false;
-  public twitterTopicAnalysis = false;
+  public socialActivity = false;
+  public keywords = false;
+  public socialReach = false;
+  public engagement = false;
+  public network = false;
+  public twitterTopic = false;
   public socialActivityDisabled = false;
-  public networkAnalysisDisabled = false;
-  public twitterTopicAnalysisDisabled = false;
+  public keywordsDisabled = false;
+  public socialReachDisabled = false;
+  public engagementDisabled = false;
+  public networkDisabled = false;
+  public twitterTopicDisabled = false;
   public occupiedSpace = 0;
+  public rowOccupied = false;
 
   constructor(private modalService: NgbModal) { }
 
@@ -28,32 +35,54 @@ export class SocialComponent implements OnInit {
   }
 
   calculateOccupiedSpace(): void {
-    if (this.socialActivityGroup) { this.occupiedSpace += 3; }
-    if (this.networkAnalysisGroup) { this.occupiedSpace += 3; }
-    if (this.twitterTopicAnalysis) { this.occupiedSpace += 3; }
+    if (this.socialActivity) { this.occupiedSpace += 2; }
+    if (this.keywords) { this.occupiedSpace += 1; }
+    if (this.socialReach) { this.occupiedSpace += 1; }
+    if (this.engagement) { this.occupiedSpace += 1; }
+    if (this.network) { this.occupiedSpace += 1; }
+    if (this.twitterTopic) { this.occupiedSpace += 3; }
   }
 
   setDisabledBtn(): void {
-    this.socialActivityDisabled = !this.socialActivityGroup && this.occupiedSpace >= 6 ? true : false;
-    this.networkAnalysisDisabled = !this.networkAnalysisGroup && this.occupiedSpace >= 6 ? true : false;
-    this.twitterTopicAnalysisDisabled = !this.twitterTopicAnalysis && this.occupiedSpace >= 6 ? true : false;
+    this.socialActivityDisabled = !this.socialActivity && this.occupiedSpace >= 3 ? true : false;
+    this.keywordsDisabled = !this.keywords && this.occupiedSpace >= 3 ? true : false;
+    this.socialReachDisabled = !this.socialReach && this.occupiedSpace >= 3 ? true : false;
+    this.engagementDisabled = !this.engagement && this.occupiedSpace >= 3 ? true : false;
+    this.networkDisabled = !this.network && this.occupiedSpace >= 3 ? true : false;
+    this.twitterTopicDisabled = !this.twitterTopic && this.occupiedSpace >= 3 ? true : false;
+    this.rowOccupied = this.occupiedSpace > 0 ? true : false;
   }
 
   onSelect(component: string): void {
     switch (component) {
       case 'socialActivity':
-        this.socialActivityGroup = !this.socialActivityGroup;
-        this.socialActivityGroup ? this.occupiedSpace += 3 : this.occupiedSpace -= 3;
+        this.socialActivity = !this.socialActivity;
+        this.socialActivity ? this.occupiedSpace += 2 : this.occupiedSpace -= 2;
         this.setDisabledBtn();
         break;
-      case 'networkAnalysis':
-        this.networkAnalysisGroup = !this.networkAnalysisGroup;
-        this.networkAnalysisGroup ? this.occupiedSpace += 3 : this.occupiedSpace -= 3;
+      case 'keywords':
+        this.keywords = !this.keywords;
+        this.keywords ? this.occupiedSpace += 1 : this.occupiedSpace -= 1;
         this.setDisabledBtn();
         break;
-      case 'twitterTopicAnalysis':
-        this.twitterTopicAnalysis = !this.twitterTopicAnalysis;
-        this.twitterTopicAnalysis ? this.occupiedSpace += 3 : this.occupiedSpace -= 3;
+      case 'socialReach':
+        this.socialReach = !this.socialReach;
+        this.socialReach ? this.occupiedSpace += 1 : this.occupiedSpace -= 1;
+        this.setDisabledBtn();
+        break;
+      case 'engagement':
+        this.engagement = !this.engagement;
+        this.engagement ? this.occupiedSpace += 1 : this.occupiedSpace -= 1;
+        this.setDisabledBtn();
+        break;
+      case 'network':
+        this.network = !this.network;
+        this.network ? this.occupiedSpace += 1 : this.occupiedSpace -= 1;
+        this.setDisabledBtn();
+        break;
+      case 'twitterTopic':
+        this.twitterTopic = !this.twitterTopic;
+        this.twitterTopic ? this.occupiedSpace += 3 : this.occupiedSpace -= 3;
         this.setDisabledBtn();
         break;
       default:
