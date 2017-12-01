634990211b9a7c8531f738617cb62126ccba392c
jessicawiradinata <jessicawiradinata@gmail.com>
2017-12-01 11:33:49 +1100

    Social container code clean up and comments

--- src/app/containers/social/social.component.ts
+++ src/app/containers/social/social.component.ts
@@ -11,32 +11,36 @@ import { CONTENTS } from '../../domain/data/mock-content';
   encapsulation: ViewEncapsulation.None
 })
 export class SocialComponent implements OnInit {
-  private content = CONTENTS[0];
+  private content = CONTENTS[0]; // Initialise content to avoid undefined errors before contents finishes loading
+  private chartDesc = 'Lorem ipsum dolor sit amet, ei mei autem docendi nostrum, esse sonet vel cu.';
+  private contentManDesc= 'Use this content manager to configure components available for selection.';
+  private isLoading = true;
+
+  // First row component attributes
   private socialActivityDisabled = false;
   private keywordsDisabled = false;
   private socialReachDisabled = false;
   private engagementDisabled = false;
   private networkDisabled = false;
   private twitterTopicDisabled = false;
+  private occupiedSpace = 0;
+  private rowFull = false;
+  private progressType = 'primary';
+  private emptyColSize = 'col-md-12';
+  private emptySpaceText = '3 of 3 spaces left';
+
+  // Second row component attributes
   private socialActivityDisabled2 = false;
   private keywordsDisabled2 = false;
   private socialReachDisabled2 = false;
   private engagementDisabled2 = false;
   private networkDisabled2 = false;
   private twitterTopicDisabled2 = false;
-  private occupiedSpace = 0;
-  private rowFull = false;
   private occupiedSpace2 = 0;
   private rowFull2 = false;
-  private progressType = 'primary';
   private progressType2 = 'primary';
-  private emptyColSize = 'col-md-12';
   private emptyColSize2 = 'col-md-12';
-  private emptySpaceText = '3 of 3 spaces left';
   private emptySpaceText2 = '3 of 3 spaces left';
-  private chartDesc = 'Lorem ipsum dolor sit amet, ei mei autem docendi nostrum, esse sonet vel cu.';
-  private contentManDesc= 'Use this content manager to configure components available for selection.';
-  private isLoading = true;
 
   constructor(private modalService: NgbModal, private contentService: ContentService) { }
 
@@ -44,6 +48,10 @@ export class SocialComponent implements OnInit {
     this.getContent(1);
   }
 
+  /**
+   * Gets page contents from Content Service and setup page after contents are fetched
+   * @param id content ID (from mock content)
+   */
   getContent(id: number): void {
     this.contentService.getContent(id).subscribe((content) => {
       this.content = content;
@@ -55,6 +63,9 @@ export class SocialComponent implements OnInit {
     });
   }
 
+  /**
+   * Updates both instructor and student contents
+   */
   updateContent(): void {
     this.contentService.updateContent(this.content).subscribe();
     // Update student content
@@ -64,6 +75,11 @@ export class SocialComponent implements OnInit {
     });
   }
 
+  /**
+   * Copies content attributes from one content to another (deep copy)
+   * @param fromContent content to be copied from
+   * @param toContent content to be copied to
+   */
   copyContent(fromContent: Content, toContent: Content): void {
     toContent.socialActivity = fromContent.socialActivity;
     toContent.keywords = fromContent.keywords;
@@ -85,10 +101,17 @@ export class SocialComponent implements OnInit {
     toContent.twitterTopicCheck = fromContent.twitterTopicCheck;
   }
 
+  /**
+   * Opens a modal
+   * @param content modal template
+   */
   open(content) {
     this.modalService.open(content);
   }
 
+  /**
+   * Calculates occupied space in the first row
+   */
   calculateOccupiedSpace(): void {
     if (this.content.socialActivity) { this.occupiedSpace += 2; }
     if (this.content.keywords) { this.occupiedSpace += 1; }
@@ -100,6 +123,9 @@ export class SocialComponent implements OnInit {
     this.setEmptySpace();
   }
 
+  /**
+   * Calculates occupied space in the second row
+   */
   calculateOccupiedSpace2(): void {
     if (this.content.socialActivity2) { this.occupiedSpace2 += 2; }
     if (this.content.keywords2) { this.occupiedSpace2 += 1; }
@@ -111,6 +137,9 @@ export class SocialComponent implements OnInit {
     this.setEmptySpace2();
   }
 
+  /**
+   * Determines disabled first row selections based on attribute checks
+   */
   setDisabledBtn(): void {
     this.socialActivityDisabled = !this.content.socialActivity && this.occupiedSpace >= 2 || this.content.socialActivity2 ? true : false;
     this.keywordsDisabled = !this.content.keywords && this.occupiedSpace >= 3 || this.content.keywords2 ? true : false;
@@ -120,6 +149,9 @@ export class SocialComponent implements OnInit {
     this.twitterTopicDisabled = !this.content.twitterTopic && this.occupiedSpace >= 1 || this.content.twitterTopic2 ? true : false;
   }
 
+  /**
+   * Determines disabled second row selections based on attribute checks
+   */
   setDisabledBtn2(): void {
     this.socialActivityDisabled2 = !this.content.socialActivity2 && this.occupiedSpace2 >= 2 || this.content.socialActivity ? true : false;
     this.keywordsDisabled2 = !this.content.keywords2 && this.occupiedSpace2 >= 3 || this.content.keywords ? true : false;
@@ -129,18 +161,27 @@ export class SocialComponent implements OnInit {
     this.twitterTopicDisabled2 = !this.content.twitterTopic2 && this.occupiedSpace2 >= 1 || this.content.twitterTopic ? true : false;
   }
 
+  /**
+   * Sets attributes of 'select your content' empty space (first row)
+   */
   setEmptySpace(): void {
     this.emptyColSize = 'col-md-' + (12 - this.occupiedSpace * 4);
     this.rowFull = this.occupiedSpace >= 3 ? true : false;
     this.emptySpaceText = (3 - this.occupiedSpace) + ' of 3 spaces left';
   }
 
+  /**
+   * Sets attributes of 'select your content' empty space (second row)
+   */
   setEmptySpace2(): void {
     this.emptyColSize2 = 'col-md-' + (12 - this.occupiedSpace2 * 4);
     this.rowFull2 = this.occupiedSpace2 >= 3 ? true : false;
     this.emptySpaceText2 = (3 - this.occupiedSpace2) + ' of 3 spaces left';
   }
 
+  /**
+   * Sets progress bar color for first row content selection modal
+   */
   setProgressType(): void {
     switch (this.occupiedSpace) {
       case 1:
@@ -157,6 +198,9 @@ export class SocialComponent implements OnInit {
     }
   }
 
+  /**
+   * Sets progress bar color for second row content selection modal
+   */
   setProgressType2(): void {
     switch (this.occupiedSpace2) {
       case 1:
@@ -173,6 +217,10 @@ export class SocialComponent implements OnInit {
     }
   }
 
+  /**
+   * Updates contents when charts are disabled or enabled (Content Manager functionality)
+   * @param component informs which checkbox is clicked
+   */
   onCheck(component: string): void {
     if (component === 'socialActivity') {
       this.content.socialActivityCheck = !this.content.socialActivityCheck;
@@ -221,6 +269,10 @@ export class SocialComponent implements OnInit {
     this.setDisabledBtn2();
   }
 
+  /**
+   * Selects or deselects first row components when content selection modal buttons are clicked
+   * @param component informs which button is clicked
+   */
   onSelect(component: string): void {
     if (component === 'socialActivity') {
       this.content.socialActivity = !this.content.socialActivity;
@@ -253,6 +305,10 @@ export class SocialComponent implements OnInit {
     this.setEmptySpace();
   }
 
+  /**
+   * Selects or deselects first row components when content selection modal buttons are clicked
+   * @param component informs which button is clicked
+   */
   onSelect2(component: string): void {
     if (component === 'socialActivity') {
       this.content.socialActivity2 = !this.content.socialActivity2;
