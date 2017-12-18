e3459add7a94ab6df6c09180f44a4805547457bd
jessicawiradinata <jessicawiradinata@gmail.com>
2017-12-18 14:04:35 +1100

    Student Task and Answer section only appear if a task is assigned

--- src/app/domain/data/mock-task.ts
+++ src/app/domain/data/mock-task.ts
@@ -8,7 +8,7 @@ export const tasks: Task[] = [
     id: 1,
     title: 'Week 1 Task',
     description: 'Lorem ipsum dolor sit amet, eos dolore aperiam admodum id, per ea nibh accusam accommodare. ' +
-    'Eos vidit antiopam constituam cu. Simul elitr in cum, usu ne dolor integre minimum.At simul sapientem per, ' +
+    'Eos vidit antiopam constituam cu. Simul elitr in cum, usu ne dolor integre minimum. At simul sapientem per, ' +
     'sit vocibus reformidans an. Probo aliquip prompta ad vis. Eum justo harum in, nihil nobis iudico ius no, minimum molestiae eos no.'
   }
 ];
